564949b7f95174f3f903bb082a0bc8478c48e3b5
Abanob Ashraf <94614795+AbanobMelk@users.noreply.github.com>
2026-05-04 22:19:37 +0300

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -35,7 +35,7 @@ BEGIN
 
 		PRINT '=> Inserting Data Into: bronze.crm_cust_info';
 		BULK INSERT bronze.crm_cust_info
-		FROM 'E:\Data Engineering\Projects\SQL Data Warehouse - Data with Baraa\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		FROM 'E:\Data Engineering\Projects\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -51,7 +51,7 @@ BEGIN
 
 		PRINT '=> Inserting Data Into: bronze.crm_prd_info';
 		BULK INSERT bronze.crm_prd_info
-		FROM 'E:\Data Engineering\Projects\SQL Data Warehouse - Data with Baraa\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		FROM 'E:\Data Engineering\Projects\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -67,7 +67,7 @@ BEGIN
 
 		PRINT '=> Inserting Data Into: bronze.crm_sales_details';
 		BULK INSERT bronze.crm_sales_details
-		FROM 'E:\Data Engineering\Projects\SQL Data Warehouse - Data with Baraa\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		FROM 'E:\Data Engineering\Projects\datasets\source_crm\sales_details.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -85,7 +85,7 @@ BEGIN
 
 		PRINT '=> Inserting Data Into: bronze.erp_cust_az12';
 		BULK INSERT bronze.erp_cust_az12
-		FROM 'E:\Data Engineering\Projects\SQL Data Warehouse - Data with Baraa\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+		FROM 'E:\Data Engineering\Projects\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -100,7 +100,7 @@ BEGIN
 
 		PRINT '=> Inserting Data Into: bronze.erp_loc_a101';
 		BULK INSERT bronze.erp_loc_a101
-		FROM 'E:\Data Engineering\Projects\SQL Data Warehouse - Data with Baraa\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+		FROM 'E:\Data Engineering\Projects\datasets\source_erp\LOC_A101.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -115,7 +115,7 @@ BEGIN
 
 		PRINT '=> Inserting Data Into: bronze.erp_px_cat_g1v2';
 		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'E:\Data Engineering\Projects\SQL Data Warehouse - Data with Baraa\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+		FROM 'E:\Data Engineering\Projects\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
